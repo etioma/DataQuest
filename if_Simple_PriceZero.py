9afa186e15b28a9c1720f572ee45f72ef9217afe
Erica Tioma <eritioma@gmail.com>
2019-11-12 11:30:34 -0300

    Create if_Simple_PriceZero.py
    estudando, if simples

---
+++ if_Simple_PriceZero.py
@@ -0,0 +1,12 @@
+'''
+In the code editor, we've already initialized the variable a_price with a value of 0. Transcribe the following sentences into code by making use of if statements:
+
+1. If a_price is equal to 0, then print the string 'This is free' (remember to use the == operator for equality).
+2. If a_price is equal to 1, then print the string 'This is not free'.
+'''
+
+a_price = 0
+if a_price == 0:
+    print('This is free')
+else:
+    print('This is not free')
